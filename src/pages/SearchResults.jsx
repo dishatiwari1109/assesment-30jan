2c81d885d0a0029be01b48468f5a00f02cf3c087
Disha@4118 <disha.tiwari181141@gmail.com>
2026-01-30 16:15:39 +0530

    disha tiwari

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,2 +1,44 @@
-import React from "react";
-// TODO: Implement search results page with grid
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "../hooks/useDebounce";
+import { searchMovies } from "../services/api";
+
+export default function SearchResults({ onSearch }) {
+  const [val, setVal] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData]=useState([]);
+  const debouncedSearchTerm = useDebounce(val, 500);
+
+  useEffect(() => {
+    if (!debouncedSearchTerm.trim()) {
+      onSearch?.({ Search: [], totalResults: "0" });
+      return;
+    }
+
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+        const dataa = await searchMovies(debouncedSearchTerm, 1);
+        console.log(dataa);
+        setData(dataa);
+        onSearch?.(dataa);
+      } catch (error) {
+        console.error("Search error:", error);
+        onSearch?.({ Search: [], totalResults: "0", error: error.message });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [debouncedSearchTerm, onSearch]);
+
+  return (
+    <>
+    {data.map((movie) => (
+      <div key={movie.imdbID}>
+        <MovieCard movie={movie} />
+      </div>
+    ))}
+    </>
+  );
+}
