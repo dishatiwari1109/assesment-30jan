f2f34c3c68216495661376afc95a3e51350096d8
Disha@4118 <disha.tiwari181141@gmail.com>
2026-02-04 13:23:19 +0530

    updated name

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { loadFromStorage } from "../services/localStorage";
+import { loadFromStorage } from "../services/localstorage";
 import MovieCard from "../components/MovieCard";
 import { searchMovies } from "../services/api";
 function Favourites (){
